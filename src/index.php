fbf144d3c10fd85ef40fe1212bff79c7a3bf7f37
matthieuGravy <matthieugravy@gmail.com>
2023-11-14 12:19:27 +0100

    intro

--- src/index.php
+++ src/index.php
@@ -1,2 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>ajout de variables</h1>
 <?php
-echo "test";
+$variable_name = "This is the variable's value";
+$us_president = 'Donald Trump';
+$birth_year = 1973;
+$is_raining = false;
+$team_players = array('Jean','Jeanne','Dirk');
+?>
+</body>
+</html>
