db6f45de16b20ce8f1c9364134a825e74e67d3dc
matthieuGravy <matthieugravy@gmail.com>
2023-11-14 12:38:11 +0100

    up

--- src/variable/humans.php
+++ src/variable/humans.php
@@ -4,5 +4,11 @@
 <?php if (isset($_GET['name'])) { ?>
     <h1>Aloha <?php echo $_GET['name']; ?>!</h1>
 <?php } ?>
+
+<?php echo $_GET['name'];
+$a = 5;
+$a = $a + 37;
+echo $a;
+?>
 </body>
 </html>
